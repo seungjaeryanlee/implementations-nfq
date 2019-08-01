30d81e0fa3fd26859f2782b197480ae041cb7f4c
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-08-01 06:50:37 +0000

    Use implementations-utils submodule

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Methods commonly used in any paper implementations."""
-from .anneal import get_linear_anneal_func  # noqa: F401
-from .logger import get_logger  # noqa: F401
-from .reproducibility import make_reproducible  # noqa: F401
-from .save_load import load_models, save_models  # noqa: F401
